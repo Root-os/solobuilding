344f44025ca2a086dea3a73f1e5d4d749b398f7e
samkiyya <samuelkiyya2012@gmail.com>
2025-03-01 02:59:52 -0800

    route is separated from index to modularize the code

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/floor', require('./floorRoute'));
+router.use('/unit', require('./unitRoute'));
+router.use('/tenant', require('./tenantRoutes'));
+router.use('/tenant-auth', require('./tenantAuthRoute'));
+router.use('/tenant-vehicle', require('./tenantVehicleRoutes'));
+router.use('/bill-type', require('./billTypeRoute'));
+router.use('/tenant-payments', require('./tenantPaymentRoute'));
+router.use('/payment-requests', require('./paymentRequestRoute'));
+router.use('/rent-collection', require('./rentCollectionRoutes'));
+router.use('/bill-payments', require('./billPaymentsRoute'));
+router.use('/parking', require('./parkingRoute'));
+router.use('/notification', require('./notificationRoute'));
+router.use('/notification-type', require('./notificationTypeRoutes'));
+router.use('/expense-type', require('./expenseTypeRoute'));
+router.use('/expense', require('./expenseRoute'));
+router.use('/complaints', require('./complaintRoutes'));
+router.use('/dashboard', require('./dashboardRoutes'));
+router.use('/withdrawal-request', require('./withdrawalRequestRoutes'));
+router.use('/email', require('./emailRoutes'));
+router.use('/auth', require('./authRoutes'));
+
+// Inventory
+router.use('/item-types', require('./itemCategory'));
+router.use('/items', require('./itemRoutes'));
+router.use('/setting', require('./settingRoutes'));
+router.use('/charging', require('./chargingRoute'));
+
+//purchases
+router.use('/api/purchases', require('./purchaseRoute'));
+router.use('/api/purchases-request', require('./purcRequestRoute'));
+router.use('/api/item-assignments', require('./itemAssignRoute'));
+router.use('/api/maintenance', require('./maintainanceRoute'));
+router.use("/api/tenant-inventory", require("./tenantInventoryRoutes"));
+router.use("/api/payment-types", require("./paymentTypeRoutes"));
+router.use("/api/service-type", require("./serviceTypeRoutes"));
+router.use("/api/payments", require("./paymentRoute"));
+router.use("/api/vendors", require("./vendorRoute"));
+router.use("/api/returns", require("./returnRoute"));
+router.use("/api/salary-payments", require("./salaryPaymentRoutes"));
+router.use("/api/stockout", require("./stockoutRoutes"));
+
+
+module.exports = router;
