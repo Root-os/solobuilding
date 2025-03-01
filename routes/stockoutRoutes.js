62509a9446b99ff832d8f8e9ad381b481a808b74
samkiyya <samuelkiyya2012@gmail.com>
2025-03-01 04:39:47 -0800

    export stockout report functionality add

--- routes/stockoutRoutes.js
+++ routes/stockoutRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const stockoutController = require("../controllers/stockoutController");
-const { verifyToken,employeeAuth,adminAuth,adminOrEmployeeAuth } = require("../middleware/auth"); // Ensure verifyToken is exported in authMiddleware
+const { employeeAuth,adminAuth,adminOrEmployeeAuth } = require("../middleware/auth"); // Ensure verifyToken is exported in authMiddleware
 
 router.post("/request", employeeAuth, stockoutController.createStockoutRequest);
 router.put('/approve/:id', adminAuth, stockoutController.approveStockout);
@@ -11,6 +11,7 @@ router.put("/:id", adminOrEmployeeAuth, stockoutController.updateStockoutRequest
 router.delete("/:id", adminAuth, stockoutController.deleteStockoutRequest);
 router.put("/cance-request/:id", employeeAuth, stockoutController.cancelStockoutRequest);
 router.get("/low-stock/check", adminAuth, stockoutController.checkLowStock);
-// router.get("/export", verifyToken, stockoutController.exportStockoutReport);
+router.get("/export", adminAuth, stockoutController.exportStockoutReport);
+router.get("/movements", adminAuth, stockoutController.getStockMovementOverview);
 
 module.exports = router;
