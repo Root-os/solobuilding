815d512ac99c28f20e2d96255737376bf056b0e7
samkiyya <samuelkiyya2012@gmail.com>
2025-03-06 02:20:44 -0800

    notify admin when tenant submit a complaint

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,15 +1,19 @@
 const Complaint =require ('../models/complaint.js');
 const Tenant=require('../models/tenant.js');
+const sendNotificationHelper= require('../helpers/sendAlert');
+const User=require('../models/user.js');
 
 // Create a new complaint with multiple image uploads
- const createComplaint = async (req, res) => {
+const createComplaint = async (req, res) => {
   try {
-    const  tenantId  = req.user.id;
-    const {  description, urgency } = req.body;
+    const tenantId = req.user.id;
+    const { description, urgency } = req.body;
     const tenant = await Tenant.findByPk(tenantId);
+
     if (!tenant) {
       return res.status(404).json({ message: 'Tenant not found' });
     }
+
     const imagePaths = req.files ? req.files.map(file => file.path) : [];
 
     if (!tenantId || !description) {
@@ -23,11 +27,29 @@ const Tenant=require('../models/tenant.js');
       images: imagePaths,
     });
 
+    const admins = await User.findAll({ where: { role: 'admin' } }); // Fetch all admins
+
+    if (complaint && admins.length > 0) {
+      // Send notification to each admin
+      await Promise.all(
+        admins.map((admin) =>
+          sendNotificationHelper({
+            adminId: admin.id,
+            title: 'New Complaint from Tenant',
+            body: `A new complaint has been submitted by ${tenant.fullName}. Please check the complaints page for more details.`,
+            type: 'New Complaint',
+            receiver_type: 'admin',
+          })
+        )
+      );
+    }
+
     res.status(201).json({ message: 'Complaint submitted successfully', complaint });
   } catch (error) {
     res.status(500).json({ message: 'Error submitting complaint', error: error.message });
   }
 };
+
 
 // Get all complaints (admin view)
  const getAllComplaints = async (req, res) => {
