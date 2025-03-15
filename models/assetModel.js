78bcee36fc4b3097c1a0264256a0b11342fe325f
Dawit Mequanent <dawitamaye@gmail.com>
2025-03-15 18:33:09 +0300

    asset auditing added

---
+++ models/assetModel.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require("../config/database"); 
+const Item = require('./item'); 
+const AssetType = require('./assetType'); 
+
+const AssetAudit = sequelize.define('AssetAudit', {
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'items', // points to the 'items' table
+        key: 'id'
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE'
+    },
+    asset_type_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'asset_types', // points to the 'asset_types' table
+        key: 'id'
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE'
+    },
+    asset_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    existing_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    damaged_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    lost_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },{
+    timestamps: true,
+    tableName: 'asset_audits'
+  });
+  
+
+// Associations
+AssetAudit.belongsTo(Item, { foreignKey: 'item_id' });
+AssetAudit.belongsTo(AssetType, { foreignKey: 'asset_type_id' });
+
+module.exports = AssetAudit;
