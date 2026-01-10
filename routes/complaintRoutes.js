617736ec067676150d2b76a35cd5ece88bf58b78
Root-Os <rodasayalewasmare@gmail.com>
2026-01-10 21:15:11 +0300

    dashboard and complaint

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -20,7 +20,7 @@ const { adminAuth, tenantAuth, } = require("../middleware/auth");
 router.post('/submit', upload.array('images', 5), tenantAuth,createComplaint);
 
 // Admin views all complaints
-router.get('/all',adminAuth, getAllComplaints);
+router.get('/all', adminAuth, getAllComplaints);
 
 // Admin assigns a complaint to a staff member
 router.put('/assign',adminAuth, assignComplaint);
@@ -39,7 +39,13 @@ router.get('/get/:complaintId', getSingleComplaint);
 
 // Get all complaints for a tenant
 router.get('/tenant',tenantAuth, getTenantComplaints);
-router.put('/update/:complaintId', tenantAuth, updateComplaint);
+router.put(
+  '/:complaintId',
+  upload.array('images', 5),
+  tenantAuth,
+  updateComplaint
+);
+
 
 // Get all complaints assigned to a staff member
 router.get('/assigned/:employeeId', getAssignedComplaints);
